40a78c9af8e492b583dd598e27733cb5d2f5734e
Луговий Павло <you@example.com>
2026-02-04 15:37:21 +0200

    Add car list view and template

--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Car
 
-# Create your views here.
+def car_list(request):
+    # Отримуємо всі машини з бази даних
+    cars = Car.objects.all()
+    # Передаємо їх у шаблон (який зараз створимо)
+    return render(request, 'cars/car_list.html', {'cars': cars})
--- config/urls.py
+++ config/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include  # <-- Додали include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('cars.urls')),  # <-- Головна сторінка тепер веде в cars
 ]
 
-# Це магія, яка дозволяє бачити фотографії під час розробки
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
